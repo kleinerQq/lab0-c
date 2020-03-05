d6eb79c447eb7707959bf951cff2665583ca3935
kleinerqq <chih.yen5409@gmail.com>
2020-03-06 02:41:56 +0800

    Improvement sort left

--- queue.c
+++ queue.c
@@ -13,8 +13,14 @@ queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
     /* TODO: What if malloc returned NULL? */
-    q->head = NULL;
-    return q;
+    if (q == NULL) {
+        return NULL;
+    } else {
+        q->head = NULL;
+        q->tail = NULL;
+        q->size = 0;
+        return q;
+    }
 }
 
 /* Free all storage used by queue */
@@ -22,6 +28,11 @@ void q_free(queue_t *q)
 {
     /* TODO: How about freeing the list elements and the strings? */
     /* Free queue structure */
+    if (q == NULL) {
+        return;
+    }
+    while (q_remove_head(q, NULL, 0))
+        ;
     free(q);
 }
 
@@ -34,14 +45,30 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
-    list_ele_t *newh;
-    /* TODO: What should you do if the q is NULL? */
-    newh = malloc(sizeof(list_ele_t));
-    /* Don't forget to allocate space for the string and copy it */
-    /* What if either call to malloc returns NULL? */
-    newh->next = q->head;
-    q->head = newh;
-    return true;
+    if (q == NULL) {
+        return false;
+    } else {
+        list_ele_t *newh;
+        newh = malloc(sizeof(list_ele_t));
+        if (newh == NULL) {
+            return false;
+        } else {
+            newh->value = malloc(strlen(s) + 1 * sizeof(char));
+            if (newh->value == NULL) {
+                free(newh);
+                return false;
+            } else {
+                strncpy(newh->value, s, strlen(s) + 1);
+                newh->next = q->head;
+                q->head = newh;
+                q->size++;
+                if (q->size == 1) {
+                    q->tail = q->head;
+                }
+                return true;
+            }
+        }
+    }
 }
 
 /*
@@ -56,7 +83,38 @@ bool q_insert_tail(queue_t *q, char *s)
     /* TODO: You need to write the complete code for this function */
     /* Remember: It should operate in O(1) time */
     /* TODO: Remove the above comment when you are about to implement. */
-    return false;
+    if (q == NULL) {
+        return false;
+    } else {
+        if (q->size == 0) {
+            return q_insert_head(q, s);
+        } else {
+            list_ele_t *newt;
+            newt = malloc(sizeof(list_ele_t));
+            if (newt == NULL) {
+                return false;
+            } else {
+                newt->value = malloc((strlen(s) + 1) * sizeof(char));
+                if (newt->value == NULL) {
+                    free(newt);
+                    return false;
+                } else {
+                    strncpy(newt->value, s, strlen(s) + 1);
+                    newt->next = NULL;
+                    q->tail->next = newt;
+                    q->tail = newt;
+                    q->size++;
+                    if (q->size == 1) {
+                        q->head = q->tail;
+                    }
+                    return true;
+                }
+
+            }
+
+        }
+
+    }
 }
 
 /*
@@ -71,8 +129,25 @@ bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
 {
     /* TODO: You need to fix up this code. */
     /* TODO: Remove the above comment when you are about to implement. */
-    q->head = q->head->next;
-    return true;
+    if (q == NULL || q->head == NULL) {
+        return false;
+    } else {
+        // because will modify q->head
+        list_ele_t *tmp = q->head;
+        if (sp != NULL) {
+            strncpy(sp, tmp->value, bufsize - 1);
+            sp[bufsize - 1] = '\0';
+        }
+        q->head = q->head->next;
+        q->size--;
+        if (q->size <= 1) {
+            // Point tail to head
+            q->tail = q->head;
+        }
+        free(tmp->value);
+        free(tmp);
+        return true;
+    }
 }
 
 /*
@@ -81,11 +156,13 @@ bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
  */
 int q_size(queue_t *q)
 {
-    /* TODO: You need to write the code for this function */
-    /* Remember: It should operate in O(1) time */
-    /* TODO: Remove the above comment when you are about to implement. */
-    return 0;
+    if (q == NULL) {
+        return 0;
+    } else {
+        return q->size;
+    }
 }
+
 
 /*
  * Reverse elements in queue
@@ -98,6 +175,22 @@ void q_reverse(queue_t *q)
 {
     /* TODO: You need to write the code for this function */
     /* TODO: Remove the above comment when you are about to implement. */
+    if (q == NULL || q->size == 0 || q->head == NULL) {
+        return;
+    } else {
+        q->tail = q->head;
+        list_ele_t *currentNode = q->head;
+        list_ele_t *tmpNextNode = q->head->next;
+
+        while (tmpNextNode) {
+            list_ele_t *tmp = tmpNextNode->next;
+            tmpNextNode->next = currentNode;
+            currentNode = tmpNextNode;
+            tmpNextNode = tmp;
+        }
+        q->head = currentNode;
+        q->tail->next = NULL;
+    }
 }
 
 /*
@@ -109,4 +202,56 @@ void q_sort(queue_t *q)
 {
     /* TODO: You need to write the code for this function */
     /* TODO: Remove the above comment when you are about to implement. */
+
+    if (q == NULL || q->size == 0 || q->head == NULL) {
+        return;
+    } else {
+
+        list_ele_t *tmpCurrentNode = q->head;
+        list_ele_t *tmpPreviousNode = NULL;
+
+        for (int i = 0; i < q->size; i++) {
+            list_ele_t *realNextNode = tmpCurrentNode->next;
+            list_ele_t *tmpCompareToNode = q->head;
+            list_ele_t *tmpPreviousCompareToNode = NULL;
+            _Bool insertFlag = false;
+            for (int j = 0; j < i; j++) {
+                int result =
+                    strcmp(tmpCompareToNode->value, tmpCurrentNode->value);
+                if (tmpCompareToNode == q->head) {
+                    if (result >= 0) {
+                        insertFlag = true;
+                        tmpPreviousNode->next = realNextNode;
+                        tmpCurrentNode->next = tmpCompareToNode;
+                        q->head = tmpCurrentNode;
+                        if (i == (q->size - 1)) {
+                            q->tail = tmpCompareToNode;
+                        }
+                        break;
+                    }
+                } else {
+                    if (result >= 0) {
+                        insertFlag = true;
+                        tmpPreviousCompareToNode->next = tmpCurrentNode;
+                        tmpCurrentNode->next = tmpCompareToNode;
+                        tmpPreviousNode->next = realNextNode;
+                        if (i == (q->size - 1)) {
+                            q->tail = tmpPreviousNode;
+                        }
+                        break;
+                    }
+                }
+                tmpPreviousCompareToNode = tmpCompareToNode;
+                tmpCompareToNode = tmpCompareToNode->next;
+            }
+            // if insert happen, the tmpPreviousNode should be the same
+            if (!insertFlag) {
+                tmpPreviousNode = tmpCurrentNode;
+            }
+            tmpCurrentNode = realNextNode;
+        }
+
+    }
+
+
 }
